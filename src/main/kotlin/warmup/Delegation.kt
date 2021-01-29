0634fb1d2b62cdacb3495cd311ebc3e41725cd10
Ariel-Isaacm <ariel.isaacm@gmail.com>
2021-01-29 16:48:07 -0700

    Delegate properties

--- src/main/kotlin/warmup/Delegation.kt
+++ src/main/kotlin/warmup/Delegation.kt
@@ -1,5 +1,7 @@
 package main.kotlin.warmup
 
+import kotlin.reflect.KProperty
+
 interface SoundBehavior {
     fun makeSound()
 }
@@ -25,4 +27,52 @@ fun main() {
     tomAraya.makeSound()
     val elvisPresley = ElvisPresley("Dancin' to the Jailhouse Rock.")
     elvisPresley.makeSound()
+
+    val e = Example()
+    println(e.p)
+    e.p = "NEW"
+
+    val lazy  = LazyExample()
+    println("lazyStr = ${lazy.lazyStr}")
+    println("= ${lazy.lazyStr}")
+
+
+    val user = User2(mapOf(
+        "name" to "John Doe",
+        "age" to 25
+    ))
+    println("name = ${user.name}, age = ${user.age}")
+}
+
+
+class Example {
+    var p: String by Delegate()
+
+    override fun toString(): String = "Example Class"
+}
+
+class Delegate() {
+    operator fun getValue(thisRef: Any?, prop: KProperty<*>): String {
+        return "$thisRef, thank you for delegating ${prop.name} to me!"
+    }
+
+    operator fun setValue(thisRef: Any?, prop: KProperty<*>, value: String) {
+        println("$value has been assigned to ${prop.name} in $thisRef")
+    }
+}
+
+class LazyExample {
+    init {
+        println("created!")
+    }
+
+    val lazyStr: String by lazy {
+        println("computed!")
+        "my lazy"
+    }
+}
+
+class User2(val map:Map<String, Any?>){
+    val name: String by map
+    val age: Int by map
 }
